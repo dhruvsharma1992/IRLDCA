cdc03fe5568a3859102bbfe60a23d885273551a3
dhruv sharma <dhruvshubham@gmail.com>
2014-09-02 11:18:26 +0530

    Compression&Decompression  v1

---
+++ src/irldca/Compression.java
@@ -0,0 +1,182 @@
+package irldca;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Compression {
+
+	public static void main(String args[]) {
+		List<Float> checkList = new LinkedList<Float>();
+		for (float i = 0.002f; i < 0.0400; i += 0.0001) {
+			for (float j = 0; j < 2; j++) {
+				checkList.add(i);
+				checkList.add(i); 
+			}
+		}
+
+		List result;
+		//for (float i = 0.1f; i < 1.0f; i += 0.1)
+			System.out.println(compress(checkList, 0.1f));
+
+	}
+
+	private static int[] buffer;
+	private static int arcoun;
+
+	@SuppressWarnings("unchecked")
+	public static List<Integer> compress(List<Float> input, float factor) {
+
+		float max = Float.MIN_VALUE, min = Float.MAX_VALUE;
+		for (float input_ : input) {
+			if (max < input_)
+				max = input_;
+
+			if (min > input_)
+				min = input_;
+		}
+		float range = max - min;
+		int numOfDigits = 0, xcount = 0;
+		numOfDigits = getNumOfDigits(range);
+		int b;
+		if (range > 1) {
+			b = numOfDigits;
+		} else {
+			b = 1 - numOfDigits;
+		}
+		float[] inputArray = new float[input.size()];
+		int j = 0;
+		for (float input_ : input) {
+			inputArray[j++] = input_;
+		}
+		float largestCommonNumber = (float) ((int) (min / Math.pow(10, b)) * Math.pow(10,
+				b));
+		for (int i = 0; i < inputArray.length; i++)
+			inputArray[i] = (float) ((inputArray[i] - largestCommonNumber) / Math.pow(10,
+					b));
+
+		max = (float) ((max - largestCommonNumber) / Math.pow(10, b));
+		min = (float) ((min - largestCommonNumber) / Math.pow(10, b));
+		float delta = b > 0 ? (max - min) / max / (float) (Math.pow(10, b)) * 2
+				* factor : (max - min) / max / (float) (Math.pow(10, -b+1)) * 2
+				* factor;
+		for (int i = 0; i < inputArray.length; i++) {
+			inputArray[i] = (int) ((inputArray[i] - min) / delta);
+
+		}
+
+		@SuppressWarnings("rawtypes")
+		List result = new LinkedList();
+		
+		
+		result.add((float) largestCommonNumber);
+		result.add(delta);
+		result.add(max);
+		result.add(b);
+		result.add(factor);
+		
+		max = (int) ((max - min) / delta);
+
+		int numOfBinDigit = 2;
+		while ((int) (max / 2) != 0) {
+			max = max / 2;
+			numOfBinDigit++;
+		}
+		numOfBinDigit++;
+		result.add(numOfBinDigit);
+		result.add(inputArray.length);
+
+		buffer = new int[32];
+		int x = 0;
+		arcoun = 0;
+		for (int i = 0; i < inputArray.length; i++) {
+			int repeatedNumber = 1;
+			while ((i < inputArray.length - 1)
+					&& inputArray[i] == inputArray[i + 1]) {
+				repeatedNumber++;
+				i++;
+			}
+
+			for (j = 0; j < repeatedNumber / 4; j++) {
+				if (xcount == 32) {
+					result.add(x);
+					x = 0;
+					xcount = 0;
+				}
+				x = x * 2 + 1;
+				xcount++;
+			}
+
+			inary(inputArray[i], numOfBinDigit);
+			for (int index = 0; index < repeatedNumber % 4 + 1; index++) {
+				for (int bufferLength = 1; bufferLength <= arcoun; bufferLength++) {
+					if (xcount == 32) {
+						result.add(x);
+						x = 0;
+						xcount = 0;
+					}
+					x = (x * 2) + buffer[31 - arcoun + bufferLength];// System.out.print(array[31-arcoun+i]);
+					xcount++;
+				}
+			}
+		}
+		arcoun = 0;
+		/*
+		 * while (xcount-- >= 0) { if (x % 2 == 0) { buffer[31 - (arcoun++)] =
+		 * 0; } else buffer[31 - (arcoun++)] = 1; x = x / 2;
+		 * 
+		 * } x = 0; for (int i = 0; i < arcoun; i++) { if (buffer[31 - arcoun +
+		 * i] == 0) { x *= 2; } else { x *= 2 + 1; } }
+		 * 
+		 * while (arcoun < 32) x = x * 2;
+		 */
+
+		for (; xcount < 32; xcount++) {
+			x *= 2;
+
+		}
+
+		result.add(x);
+		return result;
+
+	}
+
+	private static void inary(float f, int numOfBinDigit) {
+		int x = (int) f;
+		// TODO Auto-generated method stub
+		arcoun = 0;
+		buffer[31 - arcoun] = x % 2;
+		++arcoun;// System.out.print(x%2+" ");
+
+		while (x / 2 != 0) {
+			x = x / 2;
+
+			buffer[31 - arcoun] = x % 2;
+			// System.out.print(x%2+" ");//System.out.print(arcoun + " ");
+			++arcoun;
+		}
+		while (arcoun < numOfBinDigit) {
+			buffer[31 - arcoun++] = 0;// System.out.print(arcoun + " ");
+		}
+
+	}
+
+	private static int getNumOfDigits(float range) {
+		int b;
+
+		if (range > 1) {
+			b = 1;
+			range = range / 10;
+			while ((int) range > 0) {
+				range = range / 10;
+				b++;
+			}
+		} else {
+			b = 0;
+			while (range < 1) {
+				range = range * 10;
+				b++;
+			}
+		}
+		return b;
+	}
+}
---
+++ src/irldca/Decompression.java
@@ -0,0 +1,138 @@
+package irldca;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Decompression {
+	private static int[] buffer = new int[32];
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void main(String[] args) {
+		List input = new LinkedList<>();
+		input.add(0.0f);
+		input.add(0.020000001f);
+		input.add(0.3f);
+		input.add(1);
+		input.add(0.1f);
+		input.add(6);
+		input.add(8);
+		input.add(66596);
+		input.add(-1815150592);
+
+		decompress(input);
+	}
+
+	public static List<Float> decompress(List input) {
+
+		float largestCommonNumber = (float) input.get(0);
+		float delta = (float) input.get(1);
+		float max = (float) input.get(2);
+		int b = (int) input.get(3);
+		float factor = (float) input.get(4);
+		int numOfBinDigit = (int) input.get(5);
+		int inputLength = (int) input.get(6);
+
+		List<Integer> result = new LinkedList<Integer>();
+		int xcount = 0, x = 0, repeated = 0;
+
+		for (int index = 7; index < input.size(); index++) {
+			inary((int) input.get(index));
+			for (int i = 0; i < 32; i++) {
+				if (xcount == numOfBinDigit) {
+					if (repeated == 0)
+						result.add(x);
+					else {
+						for (int j = 0; j < repeated * 4; j++)
+							result.add(x);
+					}
+
+					if (result.size() == inputLength)
+						break;
+					xcount = 0;
+					repeated = 0;
+					x = 0;
+				}
+				if (xcount == 0) {
+
+					while (buffer[i] == 1) {
+						repeated++;
+						i++;
+						if (i == 32)
+							break;
+					}
+				}
+
+				if (i == 32)
+					break;
+				x = x * 2 + buffer[i];
+				xcount++;
+			}
+		}
+
+		float min = b > 0 ? (1 - delta / 2 / factor * (float) Math.pow(10, b))
+				* max : (1 - delta / 2 / factor * (float) Math.pow(10, -b+1)) * max;
+		float[] resultAr = new float[result.size()];
+
+		for (int i = 0; i < resultAr.length; i++)
+			resultAr[i] = result.get(i);
+
+		for (int i = 0; i < resultAr.length; i++) {
+			resultAr[i] = (resultAr[i]) * delta + min;
+
+		}
+		for (int i = 0; i < resultAr.length; i++)
+			resultAr[i] = (float) ((resultAr[i] * Math.pow(10, b)) + largestCommonNumber);
+
+		System.out.println();
+
+		List<Float> resultList = new LinkedList<Float>();
+		for (int i = 0; i < resultAr.length; i++)
+			resultList.add(resultAr[i]);
+
+		return resultList;
+	}
+
+	private static void inary(int x) {
+		// TODO Auto-generated method stub
+		boolean isNegative = false;
+		if (x < 0) {
+
+			x = -x;
+			isNegative = true;
+		}
+
+		int arcoun = 0;
+		buffer[31 - arcoun] = x % 2;
+		++arcoun;// System.out.print(x%2+" ");
+
+		while (x / 2 != 0) {
+			x = x / 2;
+
+			buffer[31 - arcoun] = x % 2;
+			// System.out.print(x%2+" ");//System.out.print(arcoun + " ");
+			++arcoun;
+		}
+		while (arcoun < 32) {
+			buffer[31 - arcoun++] = 0;// System.out.print(arcoun + " ");
+		}
+
+		if (isNegative) {
+
+			for (int i = 0; i < 32; i++) {
+				buffer[i] += 1;
+				buffer[i] = buffer[i] % 2;
+			}
+
+			int carry = 0;
+			buffer[31] += 1;
+			carry = buffer[31] / 2;
+			buffer[31] = buffer[31] % 2;
+			for (int i = 30; i >= 0; i--) {
+				buffer[i] += carry;
+				carry = buffer[i] / 2;
+				buffer[i] = buffer[i] % 2;
+			}
+		}
+	}
+
+}
